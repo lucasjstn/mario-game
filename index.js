ed8d8cf3011563c62a769e322a8035efdd5ef167
lucasjstn <lucas7justino@gmail.com>
2022-10-28 12:06:38 -0300

    adding 1 velocity in the y axes to push the player down

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
+//said that when you reference window object like that you dont need the window.
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
 class Player {
   constructor() {
     //initial parameters that will be altered but got to be initialized
@@ -9,11 +13,16 @@ class Player {
       y: 100,
     };
 
-    this.width = 100;
-    this.height = 100;
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
+    this.width = 30;
+    this.height = 30;
   }
 
   draw() {
+    c.fillStyle = "red";
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 }
