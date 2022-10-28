37ede6cd803f3d7f9011e5fe55e7d6d28fca857b
lucasjstn <lucas7justino@gmail.com>
2022-10-28 11:17:51 -0300

    creating player class and draw method

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
-console.log(c);
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+
+    this.width = 100;
+    this.height = 100;
+  }
+
+  draw() {
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
